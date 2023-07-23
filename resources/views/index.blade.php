4ea9983d4e30ba4e746ae35129572366fc4eea5a
Robert Quainoo <softdeveloperrob@gmail.com>
2023-07-23 00:56:35 +0000

    added a third card for accounting services

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
 </article>
 
 <div class="row mt-3">
-    <div class="col-sm-12 col-md-6 mb-3">
+    <div class="col-sm-12 col-md-4 mb-3">
         <div class="card">
             <img src="/images/it-service.jpg" class="card-img-top" alt="Image for I.T services">
             <div class="card-body">
@@ -21,11 +21,22 @@
         </div>
     </div>
 
-    <div class="col-sm-12 col-md-6 mb-3">
+    <div class="col-sm-12 col-md-4 mb-3">
         <div class="card">
             <img src="/images/bus-reg.jpg" class="card-img-top" alt="Image of Business Compliance">
             <div class="card-body">
                 <h5 class="card-title">BUSINESS COMPLIANCE</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">LEARN MORE</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-12 col-md-4 mb-3">
+        <div class="card">
+            <img src="/images/acc-service.jpg" class="card-img-top" alt="Image of Accounting service">
+            <div class="card-body">
+                <h5 class="card-title">BOOK-KEEPING SERVICE</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="btn btn-primary">LEARN MORE</a>
             </div>
