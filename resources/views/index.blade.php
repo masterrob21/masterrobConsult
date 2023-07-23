5598f0d375c495e5053f2e358afb1faf7fb47403
Robert Quainoo <softdeveloperrob@gmail.com>
2023-07-23 00:59:01 +0000

    Changed the card title from business compliance to business registration

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,7 +25,7 @@
         <div class="card">
             <img src="/images/bus-reg.jpg" class="card-img-top" alt="Image of Business Compliance">
             <div class="card-body">
-                <h5 class="card-title">BUSINESS COMPLIANCE</h5>
+                <h5 class="card-title">BUSINESS REGISTRATION</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="btn btn-primary">LEARN MORE</a>
             </div>
