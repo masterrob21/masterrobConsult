88bedbffe064b6e2fca53b073c28e478e95589e7
Robert Quainoo <softdeveloperrob@gmail.com>
2023-08-27 12:55:14 +0000

    created a contactController and add a route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('contact', [ContactController::class, 'show']);
