ccf9cb612cf0cc2845c0ded3000e762afdb51215
Robert Quainoo <softdeveloperrob@gmail.com>
2023-09-06 02:28:45 +0000

    added image to the contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,6 +7,19 @@
     padding: 20px;
 
   }
+
+  img{
+    width: 100%;
+    object-fit: cover
+    
+
+  }
+
+  .wrapper{
+    height: 30rem;
+  }
+
+ 
 </style>
 @endsection
 
@@ -18,30 +31,37 @@
 
 @section('content')
 <div class="container my-5" >
-  <div class="col-sm-12 col-md-6 offset-md-3">
-    <h2 class="text-center text-primary">Send us a message</h2>
-    <form action="https://formspree.io/f/xaygokqk" method="POST" >
-      <div class="form-group">
-        <label for="fullname">Name</label>
-        <input type="text" class="form-control" id="fullname" name="fullname" required>
+  <div >
+    <div class="row">
+      <h2 class="text-primary">Send us a message</h2>
+      <div class="col-md-6">
+        <form class="wrapper" action="https://formspree.io/f/xaygokqk" method="POST"  >
+          <div class="form-group">
+            <label for="fullname">Name</label>
+            <input type="text" class="form-control" id="fullname" name="fullname" required>
+          </div>
+    
+          <div class="form-group mt-3">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" name="email" required>
+          </div>
+          <div class="form-group mt-3">
+            <label for="phone">Phone #</label>
+            <input type="number" class="form-control" placeholder="eg. 233" id="phone" name="phone" required>
+          </div>
+          <div class="form-group mt-3">
+            <label for="message" class="control-label">Message</label>
+            <textarea name="message" id="message" cols="30" rows="5" class="form-control" required></textarea>
+          </div>
+          <div class="mt-3"><button type="submit" class="btn btn-outline-primary">Submit</button></div>
+        </form>
       </div>
-  
-      <div class="form-group mt-3">
-        <label for="email">Email</label>
-        <input type="email" class="form-control" id="email" name="email" required>
+      <div class="col-md-6" >
+        <div >
+          <img class="wrapper" src="/images/contact_image.jpg" alt="media image">
+        </div>
       </div>
-
-      <div class="form-group mt-3">
-        <label for="phone">Phone #</label>
-        <input type="number" class="form-control" placeholder="eg. 233" id="phone" name="phone" required>
-      </div>
-
-      <div class="form-group mt-3">
-        <label for="message" class="control-label">Message</label>
-        <textarea name="message" id="message" cols="30" rows="5" class="form-control" required></textarea>
-      </div>
-      <div class="mt-3"><button type="submit" class="btn btn-outline-primary">Submit</button></div>
-    </form>
+    </div>
   </div>
 </div>
 @endsection
