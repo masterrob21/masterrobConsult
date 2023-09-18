6844377551e8371c22f4cc581c07a54c2393744f
Robert Quainoo <softdeveloperrob@gmail.com>
2023-09-18 17:33:57 +0000

    added vite to the style section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,9 @@
   <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
   <!-- Styles -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  
 
+  @vite(['resources/js/app.js', 'resources/css/app.css'])
   <link rel="stylesheet" href="/app.css">
   @yield('style')
 
