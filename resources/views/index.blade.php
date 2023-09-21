7a3e169e3c14074631cdb9ae09b09eb5bebb13e4
Robert Quainoo <softdeveloperrob@gmail.com>
2023-09-21 21:30:21 +0000

    update the summaries for the various services

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,7 +24,7 @@
                 <img src="/images/it-service.jpg" class="card-img-top" alt="Image for I.T services">
                 <div class="card-body">
                     <h5 class="card-title">I.C.T SERVICES</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">I.T. services encompass a range of offerings focused on managing and optimizing technology resources within organizations. These services include infrastructure management, software development and support, network and security services, cloud computing, cybersecurity solutions, and more. Their primary goal is to ensure efficient technology utilization, enhance data security, and align technology with business objectives for improved operational effectiveness.</p>
                     <a href="#" class="btn btn-primary link_arrow">LEARN MORE</a>
                 </div>
             </div>
@@ -34,8 +34,8 @@
             <div class="card">
                 <img src="/images/bus-reg.jpg" class="card-img-top" alt="Image of Business Compliance">
                 <div class="card-body">
-                    <h5 class="card-title">BUSINESS REGISTRATION</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h5 class="card-title">COMPANY FORMATION</h5>
+                    <p class="card-text">Company formation involves the process of legally establishing a new business entity, such as a corporation, limited liability company (LLC), partnership, or sole proprietorship. This process typically includes registering the company with the appropriate government authorities, defining its structure, ownership, governance, and other essential details, depending on the legal and regulatory requirements of the jurisdiction in which it operates. The ultimate aim is to create a distinct and recognized legal entity that can engage in business activities and comply with applicable laws and regulations.</p>
                     <a href="#" class="btn btn-primary link_arrow">LEARN MORE</a>
                 </div>
             </div>
@@ -46,7 +46,7 @@
                 <img src="/images/acc-service.jpg" class="card-img-top" alt="Image of Accounting service">
                 <div class="card-body">
                     <h5 class="card-title">BOOK-KEEPING SERVICE</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">Accounting services encompass a suite of financial management solutions offered by professionals to individuals, businesses, or organizations. These services include bookkeeping, financial reporting, tax preparation, auditing, and financial analysis, aiming to ensure accurate financial records and compliance with relevant laws and regulations. The goal is to provide a clear financial picture, facilitate informed decision-making, and maintain financial transparency for stakeholders.</p>
                     <a href="#" class="btn btn-primary link_arrow">LEARN MORE</a>
                 </div>
             </div>
