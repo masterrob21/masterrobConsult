77806fac55a809171b6f97ebcaf87c6d6b8df1a0
Robert Quainoo <softdeveloperrob@gmail.com>
2023-09-21 21:01:18 +0000

    imported bootsrap js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,5 @@
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
